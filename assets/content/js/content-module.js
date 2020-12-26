2694b72253400739ee898dd7939679bf85c82875
pyPlacca <dhegreatprosperity10@gmail.com>
2020-12-26 13:57:50 +0000

    Update content-module.js

--- assets/content/js/content-module.js
+++ assets/content/js/content-module.js
@@ -6,7 +6,7 @@ function srcToFile(src){
 function showLoading(event) {
 	event.target.style.cursor = 'progress'
 	fetching = createElementFromString(`
-		<p id="ig-down-fetching" class="fetching">
+		<p class="fetching">
 			Accessing url...
 		</p>
 	`)
@@ -14,13 +14,23 @@ function showLoading(event) {
 	return fetching
 }
 
-function clearLoading(event) {
-	fetching.parentNode.removeChild(fetching)
-	event.target.style.cursor = 'unset'
+function clearLoading(elm, event) {
+	elm.parentNode.removeChild(elm)
+	event.target.style.cursor = 'pointer'
 }
 
 function createElementFromString (string) {
 	const parser = new DOMParser();
 	const parsedHtml = parser.parseFromString(string, 'text/html');
 	return parsedHtml.body.firstElementChild;
+}
+
+function createFileName(url, mimeType, ...others) {
+	url = url.split('=').pop().split('%').shift();
+	mimeType = mimeType.split('/').pop();
+	return `${others.join('&')}-${url}.${mimeType}`;
+}
+
+String.prototype.toTitleCase = function () {
+  return this[0].toUpperCase() + this.substring(1)
 }
