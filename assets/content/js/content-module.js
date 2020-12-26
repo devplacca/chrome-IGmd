4c2305c7bcb3678e51b8d30d8823a0eabe000cd0
pyPlacca <dhegreatprosperity10@gmail.com>
2020-12-26 05:04:08 +0000

    create html string parser function for easy creation of elements

--- assets/content/js/content-module.js
+++ assets/content/js/content-module.js
@@ -1,21 +1,26 @@
-function srcToFile(src, fileName, mimeType){
+function srcToFile(src){
 	// convert url to blob / file object
-    return (
-    	fetch(src)
-    	.then( res => { return res.blob() } )
-        // .then( buf => { return new File([buf], fileName, {type:mimeType}) } )
-    )
+    return fetch(src).then(res => {return res.blob()})
 }
 
 function showLoading(event) {
 	event.target.style.cursor = 'progress'
-	fetching = document.createElement('p')
-	fetching.classList.add('fetching')
-	fetching.innerText = 'Accessing url...'
+	fetching = createElementFromString(`
+		<p id="ig-down-fetching" class="fetching">
+			Accessing url...
+		</p>
+	`)
 	document.body.appendChild(fetching)
+	return fetching
 }
 
 function clearLoading(event) {
 	fetching.parentNode.removeChild(fetching)
 	event.target.style.cursor = 'unset'
+}
+
+function createElementFromString (string) {
+	const parser = new DOMParser();
+	const parsedHtml = parser.parseFromString(string, 'text/html');
+	return parsedHtml.body.firstElementChild;
 }
