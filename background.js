d3056df8297deea99008706524e84f289ef472dd
pyplacca <pyplacca@gmail.com>
2022-07-24 23:08:58 +0000

    refactor: update page url match pattern

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ chrome.runtime.onInstalled.addListener(function() {
         // That fires when a page's URL contains a 'instagram' ...
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlContains: 'instagram' },
+            pageUrl: { hostSuffix: 'instagram.com' },
           })
         ],
         // And shows the extension's page action.
